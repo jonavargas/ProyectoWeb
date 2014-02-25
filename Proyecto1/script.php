c9fc46e034b2d9d9249baa0d9f8b6f47de53338c
jonavargas <jonavargas2012@gmail.com>
2014-02-25 01:38:58 -0600

    Función getXML y validación para el csv
    Creación de la función getXML(), y validación para verificar si existe
    el archivo csv.

--- Proyecto1/script.php
+++ Proyecto1/script.php
@@ -1,15 +1,9 @@
 <?php
-function connectDB() //Función encargada de conectarse a la Base de Datos.
-{   
-    global $argv;    //Declaración de la variables.
+function getXML(){
+    global $argv;    //Declaración de variables.
     $_SERVER['argv'];
     $xml = $argv[1];
-    $Server = "";
-    $User = "";
-    $Password = "";
-    $DB = "";
-    $return ="";
-    
+
     $xml = strtolower($xml);             //Convierte a minúscula el archivo XML que se ingresa por parámetro.
     if($xml === strtolower("config.")){  //Validación de un posible ingreso de nombre de variable.
     $xml .= "xml";                       //Concatena la extensión "xml".
@@ -21,10 +15,21 @@ function connectDB() //Función encargada de conectarse a la Base de Datos.
     if($ext != "xml"){            //Validación que se encarga de verificar como fue ingresado el nombre de archivo.
     $xml .= '.xml';               //y de ser necesario le concatena la extensión completa ".xml".
     }
+    return $xml;
+}
 
-    $file = simplexml_load_file($xml);  //Se carga de leer el archivo "xml" y esté es asignado a una variable.
-    foreach($file->Connect as $item){   //Recorre el "xml" y extrae los datos necesarios para la conexión.
-        $Server = $item->Server;        //con la base de datos y los asigna a sus respectivas variables.
+function connectDB() //Función encargada de conectarse a la Base de Datos.
+{       
+    $Server = ""; //Declaración de variables.
+    $User = "";
+    $Password = "";
+    $DB = "";
+    $return = "";
+    $fileXML = getXML();
+
+    $file = simplexml_load_file($fileXML);  //Se carga de leer el archivo "xml" y esté es asignado a una variable.
+    foreach($file->Connect as $item){       //Recorre el "xml" y extrae los datos necesarios para la conexión
+        $Server = $item->Server;            //con la base de datos y los asigna a sus respectivas variables.
         $User = $item->User;
         $Password = $item->Password;
         $DB = $item->DB;
@@ -38,6 +43,12 @@ function connectDB() //Función encargada de conectarse a la Base de Datos.
 
 date_default_timezone_set("America/Costa_Rica");        //Especifica la zona horaria de "América Central/Costa Rica".
 $name_csv = date("d").date("m").date("Y");              //Asigna la fecha actual a una variable.
+
+if(!file_exists($name_csv . ".csv")){                   //Valida que el archivo csv exista para continuar con el insert en la base de datos.
+echo " Did not find any file to insert into database!!!";
+die;
+}
+
 $recordsMade = 0;                                       //Contador de registros ingresados por día.
 $csv = fopen($name_csv . ".csv", "r");                  //Abre el archivo "csv" y lo asigna a la variable "$csv".
 while (($data = fgetcsv($csv, 1000, ";")) !== FALSE) {  //Almacena los datos de una línea completa del "csv" en el array "data".
@@ -52,12 +63,12 @@ while (($data = fgetcsv($csv, 1000, ";")) !== FALSE) {  //Almacena los datos de 
             $insert = $insert . "'" . $data[$i] . "',";
     }
     
-    $insert = $insert . ");";       //Se termina de crear el insert, el cual esta almacenado en la variable "$insert".
+    $insert = $insert . ");\n";     //Se termina de crear el insert, el cual esta almacenado en la variable "$insert".
     echo $insert;                   //Muestra los datos que se insertaran en la base de datos.
-    
     $link = connectDB();            //Se almacena en la variable "$link" la función de conexión a la base de datos.
     $result = mysqli_query($link, $insert); //Se ejecuta la cadena del insert formada.
-    mysqli_close($link);            //Finaliza la conexión con la base de datos.
+    mysqli_close($link);            //Finaliza la conexión con la base de datos.    
 }
+echo "\n Is successfully inserted all registers in the database!!!";
 fclose($csv);
 ?>
